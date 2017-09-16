754578df3fdcbc24ff9669bc3c85eb2bc44af629
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-09-16 22:25:09 +0200

    get_link

--- ndh/models.py
+++ ndh/models.py
@@ -1,5 +1,7 @@
-from .utils import full_url
 from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+
+from .utils import full_url
 
 
 class Links(object):
@@ -8,3 +10,6 @@ class Links(object):
 
     def get_admin_url(self):
         return reverse(f'admin:{self._meta.app_label}_{self._meta.model_name}_change', args=self.id)
+
+    def get_link(self):
+        return mark_safe(f'<a href="{self.get_absolute_url}">{self}</a>')
