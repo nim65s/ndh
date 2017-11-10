0e9d1788b0ee7db7f3db1f24eac75957bc15b31d
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-11-10 16:17:51 +0100

    v1.2.0: add utils.enum_to_choices

--- ndh/utils.py
+++ ndh/utils.py
@@ -3,3 +3,7 @@ from django.contrib.sites.models import Site
 
 def full_url(url=''):
     return 'https://' + Site.objects.get_current().domain + url
+
+
+def enum_to_choices(enum):
+        return ((item.value, item.name) for item in list(enum))
