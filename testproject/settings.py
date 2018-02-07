71a2b685a39813e416c145dbb5a21dd33d3b39ed
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-02-07 14:52:43 +0100

    password reset

--- testproject/settings.py
+++ testproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ndh',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'testproject',
-    'ndh',
     'bootstrap3',
 ]
 
