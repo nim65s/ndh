9c4574c4bf3d526889994c0beb50cebcf7350510
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-02-17 00:28:18 +0100

    description-file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ndh',
-    version='3.0.0',
+    version='3.0.2',
     packages=['ndh'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
