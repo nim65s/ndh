0a058cf6c523365c07e29ab12edde0b35cdda527
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-12 23:11:29 +0100

    fix cherry-pick

--- testproject/tests.py
+++ testproject/tests.py
@@ -94,10 +94,6 @@ class TestNDH(TestCase):
 
         self.assertEqual(TestModel.objects.count(), 0)
 
-    def test_absolute_url_list(self):
-        instance = TestModelList.objects.create(name='Pipo 22 é@ü', moment=timezone.now())
-        self.assertEqual(instance.get_absolute_url(), reverse('testapp:testmodellists'))
-
     def test_utils(self):
         key, val, no_key, env_file = 'DJANGO_TEST_GET_ENV', 'it=works', 'KEY_WITHOUT_VAL', '.env'
         get_env(env_file)
