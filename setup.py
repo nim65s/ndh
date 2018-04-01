043ab5ac6493858c65df4ab279037fca4b955513
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-01 23:45:51 +0200

    typo

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ndh',
-    version='3.5.0',
+    version='3.5.1',
     packages=['ndh'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
