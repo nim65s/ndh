b83f99f17a1bc0b6f5b4a7da28837f6b099c304f
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-07-21 09:56:43 +0200

    fix models.W042

---
+++ testapp/apps.py
@@ -0,0 +1,8 @@
+"""Configuration for Testapp."""
+from django.apps import AppConfig
+
+
+class TestappConfig(AppConfig):
+    """Configuration for Testapp."""
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'testapp'
